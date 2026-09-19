a71fbf6576ac177429104de33a2af0998f9a99bb
KARASZI István <ikaraszi@gmail.com>
2026-09-19 16:30:42 +0200

    Add the Assisted-by line that MELPA requests

--- magit-standup.el
+++ magit-standup.el
@@ -3,6 +3,7 @@
 ;; Copyright (C) 2026 Function Artisans, Ltd.
 
 ;; Author: István Karaszi <ikaraszi@gmail.com>
+;; Assisted-by: Claude:claude-opus-5
 ;; Version: 0.2.0
 ;; Package-Requires: ((emacs "28.1") (magit "4.5.0") (transient "0.8.0"))
 ;; Keywords: tools, vc
